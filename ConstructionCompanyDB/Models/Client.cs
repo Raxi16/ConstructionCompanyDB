bdffadcb0ddc5b6d4b2874766f2a8b5ba8e24cab
Md Tusriful Rafi <ac119820@avcol.school.nz>
2023-10-18 12:26:25 +1300

    adding annotations

--- ConstructionCompanyDB/Models/Client.cs
+++ ConstructionCompanyDB/Models/Client.cs
@@ -1,8 +1,11 @@
-﻿namespace ConstructionCompanyDB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConstructionCompanyDB.Models
 {
     public class Client
     {
         public int ClientID { get; set; }
+        [Required(ErrorMessage = "Please enter name")]
         public string Name { get; set; }
         public string  Number{ get; set; }
         public string Email { get; set; }
